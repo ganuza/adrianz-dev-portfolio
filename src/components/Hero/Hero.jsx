a23d62e85d2cc84dd1934f837133d700b1691aae
Adrian Zabolitzki <adrianzab@gmail.com>
2026-03-21 10:27:48 -0600

    Implement CTA buttons in Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,6 +11,14 @@ function Hero() {
             I build thoughtful web applications using React and modern frontend
             tools.
           </p>
+          <div className="flex justify-center md:justify-start mt-10 gap-10">
+            <button className="w-auto bg-slate-900 rounded-xl px-8 py-2 cursor-pointer transition hover:bg-slate-600 hover:-translate-y-0.5 duration-200 hover:ease-in-out">
+              View My Work
+            </button>
+            <button className="w-auto border border-slate-400 rounded-xl px-8 py-2 transition cursor-pointer hover:border-slate-600 hover:bg-slate-600/50 hover:-translate-y-0.5 duration-200 hover:ease-in-out">
+              Contact Me
+            </button>
+          </div>
         </div>
       </article>
       <article className="w-full md:w-1/2 flex justify-center items-center mt-20 md:mt-0">
